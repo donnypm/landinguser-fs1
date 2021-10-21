dd1bf4fab979b49b9529a286768c9a87c14642e3
donnypm <donnypramudia04@gmail.com>
2021-10-21 12:21:26 +0700

    landing user page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.scss";
+import { Provider } from "react-redux";
+import store from "../redux/store/store";
+import Nav from "./components/Nav";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Nav />
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
